5f14089b11687950a081b66745505f4c15960e52
Veeraboina Mahesh <146333841+MaheshVeeraboina@users.noreply.github.com>
2025-11-12 00:04:01 +0530

    Add files via upload

---
+++ src/book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, title, author, isbn, genre, quantity):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.genre = genre
+        self.quantity = quantity
