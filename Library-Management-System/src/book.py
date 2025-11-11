684a919538aa28af09f50c4ed441d5e69828f96d
Veeraboina Mahesh <146333841+MaheshVeeraboina@users.noreply.github.com>
2025-11-12 00:03:25 +0530

    Delete Library-Management-System directory

--- Library-Management-System/src/book.py
+++
@@ -1,7 +0,0 @@
-class Book:
-    def __init__(self, title, author, isbn, genre, quantity):
-        self.title = title
-        self.author = author
-        self.isbn = isbn
-        self.genre = genre
-        self.quantity = quantity
--- Library-Management-System/src/borrower.py
+++
@@ -1,5 +0,0 @@
-class Borrower:
-    def __init__(self, membership_id, name, contact):
-        self.membership_id = membership_id
-        self.name = name
-        self.contact = contact
